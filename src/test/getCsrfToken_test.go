42b8c6d0fb511108148bbd6f5a6c45539003b4ae
kbc18a11 <wada121400@gmail.com>
2022-05-26 22:28:59 +0900

    セッションインスタンスの取得関数のモック化

--- src/test/getCsrfToken_test.go
+++ src/test/getCsrfToken_test.go
@@ -7,21 +7,44 @@ import (
 	"testing"
 
 	"github.com/GIT_USER_ID/GIT_REPO_ID/src/controller"
+	"github.com/gorilla/sessions"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+/*
+セッションライブラリのモック
+*/
+type MockSession struct {
+	mock.Mock
+}
+
+/*
+セッションインスタンスの取得関数のモック化
+*/
+func (mock *MockSession) Get(name string, c echo.Context) (*sessions.Session, error) {
+	return &sessions.Session{
+			ID:      "",
+			Values:  nil,
+			Options: nil,
+			IsNew:   true,
+		},
+		nil
+}
 
 /*
 レスポンステスト
 */
 func TestResponse(t *testing.T) {
-	server := echo.New()
+	e := echo.New()
 	request := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
 	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	responseRecorder := httptest.NewRecorder()
-	c := server.NewContext(request, responseRecorder)
+	c := e.NewContext(request, responseRecorder)
 	c.SetPath("/api/v1/csrf")
 
+	// sessionライブラリをモック化しないとヌルポが起きる
 	if assert.NoError(t, controller.GetCsrfToken(c)) {
 	}
 }
